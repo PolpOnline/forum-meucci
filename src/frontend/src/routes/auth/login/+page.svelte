010a3c00e5595fc195285d827a841a6a2e94fdfb
PolpOnline <git@t0mmy.anonaddy.com>
2024-11-27 22:18:32 +0100

    Update deps

--- src/frontend/src/routes/auth/login/+page.svelte
+++ src/frontend/src/routes/auth/login/+page.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="h-screen-not-navbar flex w-full items-center justify-center">
-	<Button variant="outline" href={`${API_URL}/auth/login`}>
+	<Button variant="outline" href="{API_URL}/auth/login">
 		<LogosGoogleIcon class="h-6 w-6" />
 		Login with Google
 	</Button>
