9b1a2a219d95a0dd85668a354e9c73a5b4c1c702
PolpOnline <git@t0mmy.anonaddy.com>
2025-01-05 01:41:01 +0100

    Add back button to rounds page

--- src/frontend/src/routes/admin/rounds/[event_id]/+page.svelte
+++ src/frontend/src/routes/admin/rounds/[event_id]/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import { Badge } from '$lib/components/ui/badge';
 	import LucideMapPin from '~icons/lucide/map-pin';
+	import LucideArrowLeft from '~icons/lucide/arrow-left';
 	import AdminRound from './AdminRound.svelte';
+	import { Button } from '$lib/components/ui/button';
 
 	let { data } = $props();
 
@@ -10,14 +12,22 @@
 </script>
 
 <main>
-	<h1 class="mt-5 text-center text-3xl font-bold">
-		{name}
-		<br />
-		<Badge class="pointer-events-none mt-2" variant="secondary">
-			<LucideMapPin class="mr-1 h-4 w-4" />
-			<div class="translate-y-[5%] text-sm">{room}</div>
-		</Badge>
-	</h1>
+	<div class="grid grid-cols-12">
+		<div class="col-span-2 flex items-center justify-center">
+			<Button class="text-primary" href="/admin" size="icon" variant="outline">
+				<LucideArrowLeft />
+			</Button>
+		</div>
+		<h1 class="col-span-8 mt-5 text-center text-3xl font-bold">
+			{name}
+			<br />
+			<Badge class="pointer-events-none mt-2" variant="secondary">
+				<LucideMapPin class="mr-1 h-4 w-4" />
+				<div class="translate-y-[5%] text-sm">{room}</div>
+			</Badge>
+		</h1>
+		<div class="col-span-2"></div>
+	</div>
 	<div class="w-100 mx-auto mt-5 w-[95%] max-w-[800px]">
 		{#each rounds as round (round.round)}
 			<div class="my-4">
