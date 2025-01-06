d383c7fc764b683df97f577222f0a01bc80249fe
PolpOnline <git@t0mmy.anonaddy.com>
2025-01-06 02:07:02 +0100

    Fix scroll on login page

--- src/frontend/src/routes/auth/login/+page.svelte
+++ src/frontend/src/routes/auth/login/+page.svelte
@@ -4,9 +4,9 @@
 	import { API_URL } from '$lib/api/api';
 </script>
 
-<div class="h-screen-not-navbar flex w-full items-center justify-center">
+<main class="h-screen-not-navbar flex w-full items-center justify-center">
 	<Button variant="outline" href="{API_URL}/auth/login">
 		<LogosGoogleIcon class="h-6 w-6" />
 		Login with Google
 	</Button>
-</div>
+</main>
