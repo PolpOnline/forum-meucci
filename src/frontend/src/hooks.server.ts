d3e92cd74ab1091632efc2f80fcb65eadcb97d13
PolpOnline <git@t0mmy.anonaddy.com>
2025-01-17 20:28:23 +0100

    Check user type on index load

--- src/frontend/src/hooks.server.ts
+++ src/frontend/src/hooks.server.ts
@@ -63,7 +63,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 		event.cookies.get('meucci_forum_id') ? 'logged_in' : 'logged_out'
 	) as LoginStatus;
 
-	if (requestedPath.startsWith('/auth') || requestedPath.startsWith('/system/')) {
+	if (requestedPath.startsWith('/auth')) {
 		return resolve(event, resolveOptions);
 	}
 
--- src/frontend/src/routes/+page.server.ts
+++ src/frontend/src/routes/+page.server.ts
@@ -4,6 +4,9 @@ import { getReasonPhrase, StatusCodes } from 'http-status-codes';
 import { error, redirect } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({ fetch }) => {
+	// Check the user type, if the user is a host or admin, redirect to the admin page
+	await checkUserType({ fetch });
+
 	const {
 		data,
 		response,
@@ -27,3 +30,16 @@ export const load: PageServerLoad = async ({ fetch }) => {
 		selectedActivities: data.activities
 	};
 };
+
+async function checkUserType({ fetch }: typeof load.arguments) {
+	const { data: typeInfo } = await client.GET('/user/my_type', { fetch });
+
+	// Check the user type, if the user is a host or admin, redirect to the admin page
+	if (!typeInfo) {
+		error(StatusCodes.INTERNAL_SERVER_ERROR, getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR));
+	}
+
+	if (typeInfo.user_type === 'host' || typeInfo.user_type === 'admin') {
+		redirect(StatusCodes.MOVED_TEMPORARILY, '/admin');
+	}
+}
