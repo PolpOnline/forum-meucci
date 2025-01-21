de1266e900ec864c37a5bee389221e66fb2bb075
PolpOnline <git@t0mmy.anonaddy.com>
2025-01-22 00:17:07 +0100

    Use page from $app/state

--- src/frontend/src/routes/+error.svelte
+++ src/frontend/src/routes/+error.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import { page } from '$app/stores';
 	import { Button, buttonVariants } from '$lib/components/ui/button';
 	import { cn } from '$lib/utils';
+	import { page } from '$app/state';
 </script>
 
 <div class="h-screen-not-navbar flex w-full flex-col items-center justify-center">
-	<h1 class="text-bold text-9xl">{$page.status}</h1>
-	<h2 class="text-light mt-4 text-5xl text-muted">{$page.error?.message}</h2>
+	<h1 class="text-bold text-9xl">{page.status}</h1>
+	<h2 class="text-light mt-4 text-5xl text-muted">{page.error?.message}</h2>
 
 	<Button class={cn('mt-8 text-primary', buttonVariants({ variant: 'outline' }))} href="/">
 		Go back home
