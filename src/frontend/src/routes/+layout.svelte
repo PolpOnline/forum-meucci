a43bd9ba0e98b124b4c8a8efb81af8b69dc7a617
PolpOnline <git@t0mmy.anonaddy.com>
2025-01-22 00:10:38 +0100

    Use progress bar instead of loading spinner

--- src/frontend/src/routes/+layout.svelte
+++ src/frontend/src/routes/+layout.svelte
@@ -5,14 +5,13 @@
 	import type { LayoutData } from './$types';
 	import type { Snippet } from 'svelte';
 	import { cubicIn, cubicOut } from 'svelte/easing';
-	import { afterNavigate, beforeNavigate } from '$app/navigation';
 	import { fly } from 'svelte/transition';
-	import Loader from '$lib/components/Loader.svelte';
 	import { title } from '$lib/stores/title.store';
 	import Favicon from '$lib/images/favicon.svg';
 	import { Toaster } from '$lib/components/ui/sonner/index.js';
 	import { links } from 'unplugin-fonts/head';
 	import ActivityFullDialog from '$lib/components/dialogs/ActivityFullDialog.svelte';
+	import { ProgressBar } from '@prgm/sveltekit-progress-bar';
 
 	let { data, children }: { data: LayoutData; children: Snippet } = $props();
 
@@ -23,12 +22,6 @@
 
 	const transitionIn = { easing: cubicOut, y, duration, delay };
 	const transitionOut = { easing: cubicIn, y: -y, duration };
-
-	let isLoading = $state(false);
-
-	// Show loader only when navigating between internal pages
-	beforeNavigate(({ to }) => (isLoading = !!to?.route.id));
-	afterNavigate(() => (isLoading = false));
 </script>
 
 <svelte:head>
@@ -46,11 +39,10 @@
 <ActivityFullDialog />
 
 <div data-vaul-drawer-wrapper>
+	<ProgressBar class="text-white" zIndex={100} />
+
 	<Navbar loginStatus={data.loginStatus} />
 
-	{#if isLoading}
-		<Loader />
-	{/if}
 	{#key data.pathname}
 		<div in:fly={transitionIn} out:fly={transitionOut}>
 			{@render children()}
