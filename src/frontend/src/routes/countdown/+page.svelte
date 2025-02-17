5f31d2f3376a3ab71bbf6f52e11cbee3a74184c5
PolpOnline <git@t0mmy.anonaddy.com>
2025-02-17 23:29:42 +0100

    Disable countdown for now

--- src/frontend/src/routes/countdown/+page.svelte
+++ src/frontend/src/routes/countdown/+page.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	import Countdown from './Countdown.svelte';
 
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	let { data }: { data: PageData } = $props();
 </script>
 
 <div class="h-screen-not-navbar flex flex-col items-center justify-center space-y-10 pb-20">
-	<span class="text-center text-2xl font-bold"> Le iscrizioni apriranno tra: </span>
+	<!--	<span class="text-center text-2xl font-bold"> Le iscrizioni apriranno tra: </span>-->
 
-	<Countdown timeUntil={new Date(data.startDate)} />
+	<!--	<Countdown timeUntil={new Date(data.startDate)} />-->
+	<span class="text-center text-2xl font-bold"> Le iscrizioni non sono ancora aperte </span>
 </div>
